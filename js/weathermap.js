9fb68f8eebe94e45368c6816ab4ae1e2cd472134
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-07-30 11:24:14 -0500

    minor tweaks

--- js/weathermap.js
+++ js/weathermap.js
@@ -5,9 +5,9 @@
     var coordinates = document.getElementById('coordinates');
     var map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
-        center: [-98.4936, 29.4241], // starting position [lng, lat]
-        zoom: 10 // starting zoom
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: [-98.4936, 29.4241],
+        zoom: 10
     });
 
 //Adds Navigation controls and disables scroll to zoom
@@ -19,11 +19,14 @@
         geocode('San Antonio, TX');
         });
 
+
     const geocode = function (searchInfo) {
+//Tests if the searchInfo parameters are in an array and joins using a comma
     if (Array.isArray(searchInfo)){
     searchInfo.join(",")
     }
-        // Call/Query MapBox API to obtain the response object with the necessary map information
+
+// Call/Query MapBox API to obtain the response object with the necessary map information
         $.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchInfo}.json`, {
             access_token: mapboxKey,
             types: ["place", "address"]
@@ -33,10 +36,10 @@
            var cityName = data.features[0].place_name;
            $('#city').html(cityName);
            forecastWeather(latitude, longitude)
-           // Sets Marker
+// Sets Marker
                marker.setLngLat([longitude, latitude]);
                marker.addTo(map);
-           // Moves view with Marker
+// Moves view with Marker
            map.flyTo({
            center: [longitude, latitude],
            zoom: 10,
@@ -45,6 +48,7 @@
         });
     }
 
+//Pulls info from search input/value and create marker
     function setCityMarker() {
         const city = $('#geocoder').val();
         geocode(city);
@@ -53,9 +57,7 @@
     $('#search-btn').on("click", setCityMarker)
 
 
-    //Forward Geocoding and reverse geocoding*******
-
-    //Draggable Marker
+//Draggable Marker
     var marker = new mapboxgl.Marker({
         draggable: true,
     })
@@ -70,7 +72,7 @@
     marker.on('dragend', onDragEnd);
 
 
-//Today's Weather/Banner
+//Today's Weather/Banner/Jumbotron
     const forecastWeather = function (latitude, longitude) {
         $.get("https://api.openweathermap.org/data/2.5/onecall", {
             APPID: openWeatherKey,
@@ -78,7 +80,6 @@
             lon: longitude,
             units: "imperial",
         }).done(function (data) {
-            // console.log(data)
             let date = new Date(data.daily[0].dt * 1000);
             let highTemp = data.daily[0].temp.max;
             let lowTemp = data.daily[0].temp.min;
@@ -87,11 +88,10 @@
             $('.main-weather').empty();
             $('.main-weather').append(
                 '<div class="bigger-text"> Weather for ' + date.toDateString() + '</div>'
-                + '<ul><div id="daily-temp"><li>' + currentTemp.toFixed(1) + '°F</li>'
+                + '<ul><div id="daily-temp"><li class="mt-3">' + currentTemp.toFixed(1) + '°F</li>'
                 + '<li><img src=' + icon + '></li></div>'
-                + '<li> Low: ' + lowTemp.toFixed(1) + "°F / High: " + highTemp.toFixed(1) + "°F with " + data.daily[0].weather[0].description + '</li>'
-                + '<li><strong> Feels Like: </strong> ' + data.daily[0].feels_like.day + "°F" + '</span></li>'
-                + '<li id="quote"><em></em></li></ul>')
+                + '<li>Low: ' + lowTemp.toFixed(1) + "°F / High: " + highTemp.toFixed(1) + "°F with " + data.daily[0].weather[0].description + '</li>'
+                + '<li><strong>Feels Like: </strong> ' + data.daily[0].feels_like.day + "°F" + '</span></li>')
         });
 //Week's Weather/Cards
         $.get("http://api.openweathermap.org/data/2.5/onecall", {
@@ -107,14 +107,14 @@
                 let lowTemp = data.daily[i].temp.min;
                 const icon = `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png`
                 $('.daily-forecast').append('<div class="mr-5 card">' +
-                    '<div id="weather-date" class="card-header text-center">' + date.toDateString() + '</div>'
+                    '<div id="weather-date" class="card-header text-center font-weight-bold">' + date.toDateString() + '</div>'
                     + '<ul class="list-group list-group-flush">'
-                    + '<li class="list-group-item text-center">' + lowTemp.toFixed(1) + "°F / " + highTemp.toFixed(1) + "°F" + '</li>'
-                    + '<li class="text-center"><strong><img src=' + icon + ' alt=""></li>'
-                    + '<li class="list-group-item text-center"><strong> Description: </strong><br>' + data.daily[i].weather[0].description + '</li>'
-                    + '<li class="list-group-item text-center"><strong> Humidity: </strong>' + data.daily[i].humidity + '%' + '</li>'
-                    + '<li class="list-group-item text-center"><strong> Wind: </strong>' + data.daily[i].wind_speed + ' mph</li>'
-                    + '<li class="list-group-item text-center"><strong> Pressure: </strong>' + data.daily[i].pressure + ' mmHg</li>'
+                    + '<li class="list-group-item">' + lowTemp.toFixed(1) + "°F / " + highTemp.toFixed(1) + "°F" + '</li>'
+                    + '<li class="text-center"><img src=' + icon + ' alt=""></li>'
+                    + '<li class="list-group-item"><strong> Description: </strong><br>' + data.daily[i].weather[0].description + '</li>'
+                    + '<li class="list-group-item"><strong> Humidity: </strong>' + data.daily[i].humidity + '%' + '</li>'
+                    + '<li class="list-group-item"><strong> Wind: </strong>' + data.daily[i].wind_speed + ' mph</li>'
+                    + '<li class="list-group-item"><strong> Pressure: </strong>' + data.daily[i].pressure + ' mmHg</li>'
                     + '</ul>' + '</div>'
                 )
             }
